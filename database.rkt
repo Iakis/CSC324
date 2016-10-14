21efa35f08fa4a076b1e42b91eeb4be58778b423
Justin <c3lisika@gmail.com>
2016-10-14 01:44:50 -0400

    starter code with part 0

---
+++ database.rkt
@@ -0,0 +1,121 @@
+#| Assignment 1 - Racket Query Language  (due Oct 14, 11:50pm)
+
+***Write the names, CDF accounts and student IDs for each of your group members below.***
+<Name>, <CDF>, <ID>
+<Name>, <CDF>, <ID>
+|#
+#lang racket
+
+; Function versions for common syntactic forms.
+; *Use these in your queries instead of the syntactic forms!!!*
+;(define (And x y) (and x y)) 
+;(define (Or x y) (or x y))
+;(define (If x y z) (if x y z))
+;
+; Correction Oct 5 2016
+
+(define-syntax If
+  (syntax-rules ()
+  ((If a b c)
+  (if a b c))))
+
+(define-syntax Or
+  (syntax-rules ()
+  ((Or a b c)
+  (or a b c))))
+
+(define-syntax And
+  (syntax-rules ()
+  ((And a b c)
+  (and a b c))))
+; Please do define And, Or as syntactic forms
+; We have actually done this in class you may use the class code and this week's lab code for this.
+  
+
+; TODO: After you have defined your macro(s), make sure you add each one
+; to the provide statement.
+(provide attributes
+         tuples
+         size)
+
+; Part 0: Semantic aliases
+
+#|
+(attributes table)
+  table: a valid table (i.e., a list of lists as specified by assigment)
+
+  Returns a list of the attributes in 'table', in the order they appear.
+|#
+(define attributes car)
+
+#|
+(tuples table)
+  table: a valid table
+
+  Returns a list of all tuples in 'table', in the order they appear.
+  Note: it is possible for 'table' to contain no tuples.
+|#
+(define tuples cdr)
+
+#|
+(size table)
+  table: a valid table
+
+  Returns the number of tuples in 'table'.
+|#
+(define (size x) 
+  (length (tuples x)))
+
+#|------------------------------------------------------------------------------------------------------------- |#
+#|------------------------------------------------------------------------------------------------------------- |#
+#|------------------------------------------------------------------------------------------------------------- |#
+
+; Part I "WHERE" helpers; you may or may not wish to implement these.
+
+#|
+A function that takes: 
+  - a list of attributes
+  - a string (representing an attribute)
+  - a tuple
+
+  and returns the value of the tuple corresponding to that attribute.
+|#
+
+#|
+A function that takes:
+  - f: a unary function that takes a tuple and returns a boolean value
+  - table: a valid table
+
+  and returns a new table containing only the tuples in 'table'
+  that satisfy 'f'.
+|#
+
+#|
+A function 'replace-attr' that takes:
+  - x 
+  - a list of attributes
+
+  and returns a function 'f' which takes a tuple and does the following:
+    - If 'x' is in the list of attributes, return the corrresponding value 
+      in the tuple.
+    - Otherwise, just ignore the tuple and return 'x'.
+|#
+
+
+#|------------------------------------------------------------------------------------------------------------- |#
+#|------------------------------------------------------------------------------------------------------------- |#
+#|------------------------------------------------------------------------------------------------------------- |#
+
+; Starter for Part 3; feel free to ignore!
+
+; What should this macro do?
+(define-syntax replace
+  (syntax-rules ()
+    ; The recursive step, when given a compound expression
+    [(replace (expr ...) table)
+     ; Change this!
+     (void)]
+    ; The base case, when given just an atom. This is easier!
+    [(replace atom table)
+     ; Change this!
+     (void)]))
